ba9e6f8d73bf30c17ae64e6b066241e53f7ffd8a
kimberly <kimberly.espinosa.deguzman@gmai.com>
2021-05-24 10:50:56 +0800

    Version 1 by Kalvin Kyle Aranas

---
+++ script2.py
@@ -0,0 +1,35 @@
+name = "Kalvin Kyle Aranas"
+cs = "SBIR-3K"
+cn = "09978675644356"
+ea = "kalvinkylearanas@gmail.com"
+
+print("\n" "Name:" + name )
+print("Course and Section:" + cs)
+print("Contact number:" + cn)
+print("Email Address:" + ea)
+
+
+print("   |      |     ")
+print(" X |   X  |  O  ")
+print("---+  --- + --- ")
+print(" O |   O  |     ")
+print("---+  --- + --- ")
+print(" X |      |     ")
+print("   |      |     ")
+
+
+
+print("M      M   FFFFFF")
+print("M  M M M   F     ")
+print("M  M   M   FFFF  ")
+print("M      M   F     ")
+print("M      M   F     ")
+
+print("""XXXXXXXXXXXXXXXXX'
+X               X
+X    _     _    X'
+X    0     0    X'
+X       ^       X'
+X   \ \_____/   X'
+X               X'
+XXXXXXXXXXXXXXXXX'""") 
